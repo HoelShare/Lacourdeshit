5d87c991fcf5b6a970959bc18f343efc19c2150d
moritz <moritztec@protonmail.com>
2017-09-11 18:14:56 +0200

    []

--- ibcao_login.py
+++ ibcao_login.py
@@ -3,6 +3,7 @@ import requests
 def get_token():
     resp = requests.get("https://instagram.com")
     csfrtoken = resp.cookies['csrftoken']
-    print csfrtoken
+    return csfrtoken
+
 if __name__ == "__main__":
-    get_token()
+    print get_token()
